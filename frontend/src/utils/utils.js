fcedbf7df33f06169cff7ee51c176ebf0f387175
Kate Danilenko <keitilins@yandex.ru>
2023-02-08 21:21:43 +0300

    frontend

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-52',
+  baseUrl: 'http://mesto.ketrindan.nomoredomainsclub.ru',
   headers: {
     authorization: '00a673b9-8570-48a8-8e0e-9115399f6375',
     'Content-Type': 'application/json'
@@ -8,7 +8,7 @@ export const apiConfig = {
 
 
 export const authConfig = {
-  baseUrl: 'https://auth.nomoreparties.co',
+  baseUrl: 'http://mesto.ketrindan.nomoredomainsclub.ru',
   headers: {
     'Content-Type': 'application/json'
   }
