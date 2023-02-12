129b0d26d2df349dbb0ec0ce751a68cc001dba27
Kate Danilenko <keitilins@yandex.ru>
2023-02-12 14:27:37 +0300

    add forms validation

--- frontend/src/components/EditAvatarPopup.js
+++ frontend/src/components/EditAvatarPopup.js
@@ -1,14 +1,23 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import PopupWithForm from "./PopupWithForm";
+import useForm from "../hooks/useForm";
 
 function EditAvatarPopup(props) {
-	const avatarRef = useRef();
+	const { values, errors, isValid, onChange, resetForm } = useForm({
+    avatar: "",
+  });
+
+	values.avatar = useRef();
 
 	function handleSubmit(e) {
 		e.preventDefault();
 	
-		props.onUpdateAvatar({avatar: avatarRef.current.value});
+		props.onUpdateAvatar({avatar: values.avatar.current.value});
 	} 
+
+  useEffect(() => {
+    resetForm();
+  }, [props.isOpen, resetForm]);
 
 	return (
 		<PopupWithForm 
@@ -16,13 +25,14 @@ function EditAvatarPopup(props) {
       title="Обновить аватар"
       children={<>
         <input type="url" className="form__input form__input_type_avatar" id="avatar-input" name="avatar" 
-					placeholder="Ссылка на аватар" required ref={avatarRef}/>
-        <span className="form__input-error avatar-input-error"></span>
+					placeholder="Ссылка на аватар" required onChange={onChange} ref={values.avatar}/>
+        <span className={"form__input-error avatar-input-error " + (errors.avatar ? "form__input-error_active" : "")}>{errors.avatar}</span>
       </>}
       buttonText={props.onLoading ? "Сохранение..." : "Сохранить"}
       isOpen={props.isOpen}
       onClose={props.onClose}
 			onSubmit={handleSubmit}
+      isValid = {isValid}
     />
 	)
 }
